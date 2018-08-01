7a4d37cf6a446354b31e7038ac090655cec2b632
maixianyu <maixianyu@qq.com>
2018-08-01 10:27:35 +0800

    finish week2 day1

--- src/four-clojure/part1.clj
+++ src/four-clojure/part1.clj
@@ -6,6 +6,43 @@
   (println x "Hello, World!"))
 
 
+; problem26
+(defn fibonacci-sequaence [n]
+  (loop [res [1]
+         x0 0
+         x1 1
+         cnt 1]
+    (if (= cnt n)
+      (seq res)
+      (recur (conj res (+ x0 x1))
+             x1
+             (+ x0 x1)
+             (+ cnt 1)))))
+
+(fibonacci-sequaence 8)
+
+; problem 29
+(def chac-vec
+  (set (map char
+       (range 65 91))))
+chac-vec
+
+(defn get-the-caps [str-arg]
+  (def chac-vec
+    (set (map char
+         (range 65 91))))
+  (loop [res []
+         s (seq str-arg)]
+    (if (empty? s)
+      (apply str res)
+      (recur (if (contains? chac-vec (first s))
+               (conj res (first s))
+               res)
+             (next s)))))
+
+(get-the-caps "HeLlO, WoRlD!")
+
+
 ; problem30
 (defn delrep [x]
   (loop [res []
@@ -65,5 +102,24 @@
 
 (drop-every-Nth-item [1 2 3 4 5] 2)
 
-;problem45
+; problem42
+(defn factorial-fun [x]
+  (loop [res 1
+         cur-x x]
+    (if (= cur-x 1)
+      res
+      (recur (* res cur-x)
+             (- cur-x 1)))))
+
+(factorial-fun 3)
+
+; problem45
 (= (seq (list 1 4 7 10 13)) (take 5 (iterate #(+ 3 %) 1)))
+
+
+; problem48
+6
+(when true 6)
+
+; problem52
+(= [2 4] (let [[a b c d e f g] (range)] [c e]))
